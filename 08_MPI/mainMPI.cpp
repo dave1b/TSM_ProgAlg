b5f5c63deee4a3e52081cc3699b183c539716574
Dave Brunner <dave.brunner@protonmail.ch>
2024-04-22 07:29:55 +0200

    week 8

--- 08_MPI/mainMPI.cpp
+++ 08_MPI/mainMPI.cpp
@@ -4,22 +4,27 @@
 #include "mpi.h"
 
 //////////////////////////////////////////////////////////////////////////////////////////////////
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
 	int p, id;
 
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &p);
 	MPI_Comm_rank(MPI_COMM_WORLD, &id);
 
-	if (id == 0) {
+	if (id == 0)
+	{
 		std::array<char, 50> greeting;
 
 		std::cout << "Process " << id << " receives greetings from " << p - 1 << " processes!" << std::endl;
-		for (int i = 1; i < p; i++) {
+		for (int i = 1; i < p; i++)
+		{
 			// TODO use MPI_Recv
 		}
 		std::cout << "Greetings, done!" << std::endl;
-	} else {
+	}
+	else
+	{
 		// TODO use MPI_Send
 	}
 
